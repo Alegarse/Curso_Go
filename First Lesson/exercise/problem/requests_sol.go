90d47f6ce0ebf52370ac7d29d9c1c3008b2bdc73
Alejandro Garcia <agscm2@gmail.com>
2020-07-02 18:24:02 +0200

    Uploading

--- First Lesson/exercise/problem/requests_sol.go
+++ First Lesson/exercise/problem/requests_sol.go
@@ -9,12 +9,11 @@ import (
 )
 
 func main() {
-
 	var wg sync.WaitGroup
 
 	sites := []string{
 		"https://www.google.com",
-		"https://drive.google.com",
+		"https://drive.gogle.com", // there's a missing 'o' in google!!
 		"https://maps.google.com",
 		"https://hangouts.google.com",
 	}
@@ -40,6 +39,9 @@ func main() {
 				io.WriteString(os.Stdout, res.Status+"\n")
 			}
 		}(site)
+
+		// time.Sleep(time.Second)
 	}
+
 	wg.Wait()
 }
